ff3c696755d35c3fea610f48c065a82db970ceae
LALTECH\abubakar_291 <muhammadabubakarkhan1234@gmail.com>
2026-04-14 13:22:41 +0500

    Refactor Sales Management Models: Improve Data Annotations and Navigation Properties

---
+++ SalesManagementSystem/Data/AppDbContext.cs
@@ -0,0 +1,120 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using SalesManagementSystem.Models;
+//namespace SalesManagementSystem.Data
+//{
+//    public class AppDbContext : DbContext
+//    {
+//        public AppDbContext(DbContextOptions<AppDbContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        public DbSet<SaleAcct> SaleAccts { get; set; }
+//        public DbSet<SaleProduct> SaleProducts { get; set; }
+//        public DbSet<SalePlatform> SalePlatforms { get; set; }
+//        public DbSet<SaleTransactionType> SaleTransactionTypes { get; set; }
+//        public DbSet<SaleAccount> SaleAccounts { get; set; }
+//        public DbSet<SaleCharge> SaleCharges { get; set; }
+//        public DbSet<SaleChargeType> SaleChargeTypes { get; set; }
+//        public DbSet<SaleStatus> SaleStatuses { get; set; }
+//    }
+//}
+
+using Microsoft.EntityFrameworkCore;
+using SalesManagementSystem.Models;
+
+namespace SalesManagementSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SaleAcct> SaleAccts { get; set; }
+        public DbSet<SalePlatform> SalePlatforms { get; set; }
+        public DbSet<SaleProduct> SaleProducts { get; set; }
+        public DbSet<SaleTransactionType> SaleTransactionTypes { get; set; }
+        public DbSet<SaleAccount> SaleAccounts { get; set; }
+        public DbSet<SaleStatus> SaleStatuses { get; set; }
+        public DbSet<SaleChargeType> SaleChargeTypes { get; set; }
+        public DbSet<SaleCharge> SaleCharges { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Match existing SQL Server table names (dbo.*)
+            modelBuilder.Entity<SaleAcct>().ToTable("SaleAcct");
+            modelBuilder.Entity<SalePlatform>().ToTable("SalePlatform");
+            modelBuilder.Entity<SaleProduct>().ToTable("SaleProduct");
+            modelBuilder.Entity<SaleTransactionType>().ToTable("SaleTransactionType");
+            modelBuilder.Entity<SaleAccount>().ToTable("SaleAccount");
+            modelBuilder.Entity<SaleStatus>().ToTable("SaleStatus");
+            modelBuilder.Entity<SaleChargeType>().ToTable("SaleChargeType");
+            modelBuilder.Entity<SaleCharge>().ToTable("SaleCharge");
+
+            // Computed column (PERSISTED)
+            modelBuilder.Entity<SaleAcct>()
+                .Property(p => p.ProfitAmount)
+                .HasComputedColumnSql(
+                    "ISNULL(SoldAmount,0) - ISNULL(CostAmount,0) - ISNULL(TotalProCharges,0) - ISNULL(AmazonFee,0) - ISNULL(OtherCharges,0) + ISNULL(TotalRroRebate,0)",
+                    stored: true);
+
+            // Relationships
+            modelBuilder.Entity<SaleProduct>()
+                .HasOne(p => p.Platform)
+                .WithMany(p => p.Products)
+                .HasForeignKey(p => p.PlatformId);
+
+            modelBuilder.Entity<SaleAccount>()
+                .HasOne(a => a.Platform)
+                .WithMany()
+                .HasForeignKey(a => a.PlatformId);
+
+            modelBuilder.Entity<SaleAcct>()
+                .HasOne(s => s.Platform)
+                .WithMany()
+                .HasForeignKey(s => s.PlatformId);
+
+            modelBuilder.Entity<SaleAcct>()
+                .HasOne(s => s.Product)
+                .WithMany()
+                .HasForeignKey(s => s.ProductId);
+
+            modelBuilder.Entity<SaleAcct>()
+                .HasOne(s => s.TransactionType)
+                .WithMany()
+                .HasForeignKey(s => s.TransactionId);
+
+            modelBuilder.Entity<SaleAcct>()
+                .HasOne(s => s.FromAccount)
+                .WithMany()
+                .HasForeignKey(s => s.FromAccountId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<SaleAcct>()
+                .HasOne(s => s.ToAccount)
+                .WithMany()
+                .HasForeignKey(s => s.ToAccountId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<SaleAcct>()
+                .HasOne(s => s.StatusMaster)
+                .WithMany()
+                .HasForeignKey(s => s.StatusID);
+
+            modelBuilder.Entity<SaleCharge>()
+                .HasOne(c => c.Sale)
+                .WithMany(s => s.Charges)
+                .HasForeignKey(c => c.SaleId);
+
+            modelBuilder.Entity<SaleCharge>()
+                .HasOne(c => c.ChargeType)
+                .WithMany()
+                .HasForeignKey(c => c.ChargeTypeId);
+        }
+
+    }
+}
